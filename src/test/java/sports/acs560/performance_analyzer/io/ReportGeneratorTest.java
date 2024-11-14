be5dd5535044015fd6d8cc2cd798bf4c0ef789cc
HarshithaGudapati <gudapatiharshitha8247@gmail.com>
2024-11-14 00:00:46 -0500

    Update ReportGeneratorTest.java

--- src/test/java/sports/acs560/performance_analyzer/io/ReportGeneratorTest.java
+++ src/test/java/sports/acs560/performance_analyzer/io/ReportGeneratorTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.io.TempDir;
 
 import sports.acs560.performance_analyzer.model.Analysis;
